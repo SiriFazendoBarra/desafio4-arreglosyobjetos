8c025363852ad30ef08195bf9eab0d8e82954a87
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2022-10-27 23:07:14 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let html = ''
+
+cantidadCuartos.value = 1
+minMetros.value = 0
+maxMetros.value = 9999
+
+cantidad = parseInt(cantidadCuartos.value)
+min = parseInt(minMetros.value)
+max = parseInt(maxMetros.value)
+cargar(cantidad, min, max)
---
+++ assets/js/variables.js
@@ -0,0 +1,11 @@
+const cantidadCuartos = document.getElementById('cantidadCuartos')
+const minMetros = document.getElementById('minMetros')
+const maxMetros = document.getElementById('maxMetros')
+const spanTitle = document.getElementById('spanTitle')
+const btnBuscar = document.getElementById('btnBuscar')
+const btnReiniciar = document.getElementById('btnReiniciar')
+const divPropiedades = document.getElementById('divPropiedades')
+
+let cantidad
+let min
+let max
